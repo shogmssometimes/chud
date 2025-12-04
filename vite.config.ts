b4ce901891cf2c01f8981832d32b203268a423f9
shogmssometimes <shoi.productions@gmail.com>
2025-12-04 08:22:07 -0600

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/chud/' : '/',
   plugins: [],
   server: {
     port: 5173
